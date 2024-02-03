680ed83d5a880c9c86e07cab8c5b1f8a509ef87e
ArjunSamuderaAhliFikri <arjunsaf717@gmail.com>
2024-02-03 12:46:56 +0700

    membuat fungsi deklarasi yang mengembalikan nama dan umur

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+function findEvenIndex(arr) {
+    let result = [];
+    arr.map(num => result.push(num * 1));
+    return result.reverse();
+}
+
+console.log(findEvenIndex([2, 4, 5, 6, 1, 4]));
+
+// Mencari kata terpendek
+function findShort(nameUser) {
+    const nama = nameUser.split(' ')
+    .reduce((firstWord, secondWord) => firstWord.length <= secondWord.length ? firstWord : secondWord);
+    return nama;
+}
+console.log(findShort('Arjun Samudera Ahli Fikri')); // Ahli
+
+function getNameUser(name, age) {
+    return `Halo, nama saya ${name}, dan umur saya ${age} tahun`;
+}
+console.log(getNameUser('Arjun', 17));
